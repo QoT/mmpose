41544ceee128ab263c31223b375d940972c835e4
xiaohang <xiaohang@sensetime.com>
2020-08-07 13:21:27 +0800

    add topdown_crowdpose_dataset

--- mmpose/datasets/datasets/__init__.py
+++ mmpose/datasets/datasets/__init__.py
@@ -1,10 +1,9 @@
 from .bottom_up import BottomUpCocoDataset
-from .top_down import (TopDownCocoDataset, TopDownMpiiDataset,
+from .top_down import (TopDownCocoDataset, TopDownMpiiDataset, TopDownCrowdPoseDataset,
                        TopDownMpiiTrbDataset, TopDownOCHumanDataset,
                        TopDownOneHand10KDataset)
 
 __all__ = [
     'TopDownCocoDataset', 'BottomUpCocoDataset', 'TopDownMpiiDataset',
     'TopDownMpiiTrbDataset', 'TopDownOneHand10KDataset',
-    'TopDownOCHumanDataset'
-]
+    'TopDownOCHumanDataset', 'TopDownCrowdPoseDataset'
