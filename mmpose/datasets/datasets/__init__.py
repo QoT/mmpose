ea7900f7364e9ab56a298b3ec3bf882cdf32623c
lizz <363664470@qq.com>
2020-08-18 10:21:28 +0800

    format
    Signed-off-by: lizz <363664470@qq.com>

--- mmpose/datasets/datasets/__init__.py
+++ mmpose/datasets/datasets/__init__.py
@@ -1,9 +1,10 @@
 from .bottom_up import BottomUpCocoDataset
-from .top_down import (TopDownCocoDataset, TopDownMpiiDataset, TopDownCrowdPoseDataset,
-                       TopDownMpiiTrbDataset, TopDownOCHumanDataset,
-                       TopDownOneHand10KDataset)
+from .top_down import (TopDownCocoDataset, TopDownCrowdPoseDataset,
+                       TopDownMpiiDataset, TopDownMpiiTrbDataset,
+                       TopDownOCHumanDataset, TopDownOneHand10KDataset)
 
 __all__ = [
     'TopDownCocoDataset', 'BottomUpCocoDataset', 'TopDownMpiiDataset',
     'TopDownMpiiTrbDataset', 'TopDownOneHand10KDataset',
     'TopDownOCHumanDataset', 'TopDownCrowdPoseDataset'
+]
